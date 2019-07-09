915f08df3cab74e5afc57a4ccedf9cf4598203b1
QuinB6248 <4_sAfJO25N@outlook.com>
2019-07-09 10:38:00 +0200

    Auth router working with bcrypt

--- Auth/router.js
+++ Auth/router.js
@@ -2,39 +2,57 @@ const { Router } = require('express')
 const { toJWT } = require('./jwt')
 const bcrypt = require('bcrypt')
 const User = require('../User/model')
+const auth = require('./middleware')
 
 const router = new Router()
 
 router.post('/login', (req, res, next) => {
-  const { name, password } = req.body
-  if(name && password){
+  const name = req.body.name
+  const password = req.body.password
+ 
+  if (!name || !password) {
+    res.status(400).send({
+      message: 'Please supply a valid name and password'
+    })
+  }
+
+  else {
     User
       .findOne({
-        where: {name: name}
+        where: {
+          name: req.body.name
+        }
       })
       .then(entity => {
-        console.log('ENTITY', entity.password)
-        if(!entity) {
-          res.status(400).send({message: 'name is incorrect'})
-        if(bcrypt.compareSync(req.body.password, entity.password)) {
-          res.send({jwt: toJWT({ userId: entity.id }) })
-        }else {
-          res.status(400).send({message: 'Password was incorrect'})
+        if (!entity) {
+          res.status(400).send({
+            message: 'Name or password was incorrect'
+          })
         }
-      }
-        //res.send({jwt: toJWT({userId: entity.id})})
+        if (bcrypt.compareSync(req.body.password, entity.password)) {
+          res.send({
+            jwt: toJWT({ userId: entity.id })
+          })
+        }
+        else {
+          res.status(400).send({
+            message: 'Name or password was incorrect'
+          })
+        }
       })
-      .catch(error => next(error))
-
-  }else {
-    res.status(400).send({message: 'please supply a valid name' })
+      .catch(error => {
+        console.error(error)
+        res.status(500).send({
+          message: 'Oops! Something went wrong'
+        })
+      })
   }
-})
-
+ })
+ 
 router.post('/signup', (req, res, next) => {
   const user = {
     name: req.body.name,
-    password: bcrypt.hashSync(req.body.password,)
+    password: bcrypt.hashSync(req.body.password, 10)
   }
   if(!req.body.name || !req.body.password) {
     res.status(404).json({message: 'password or email unknown'}).end()
@@ -48,5 +66,13 @@ router.post('/signup', (req, res, next) => {
     
   }
 })
+
+//test
+router.get('/secret-endpoint', auth, (req, res) => {
+  res.send({
+    message: `You are visiting the secret endpoint ${req.user.name}.`,
+  })
+})
+
 
 module.exports = router
